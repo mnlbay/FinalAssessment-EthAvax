65ce47311169297fcfae82dd23e1ca73afd45873
AlexanderBombais <124431442+mnlbay@users.noreply.github.com>
2024-06-13 15:48:38 +0800

    Update and rename EcommerceToken.sol to DegenToken.sol

--- DegenToken.sol
+++ DegenToken.sol
@@ -4,25 +4,27 @@ pragma solidity ^0.8.0;
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
 
-contract EcommerceToken is ERC20, Ownable {
+contract DegenToken is ERC20, Ownable {
 
-    constructor() ERC20("EcommerceToken", "ECT") Ownable(msg.sender) {}
+    // Constructor to initialize the token with a name and symbol
+    constructor() ERC20("DegenCoin", "DC") Ownable(msg.sender) {}
 
-
+    // Function to mint new tokens, can only be called by the owner
     function mint(address to, uint256 amount) public onlyOwner {
         _mint(to, amount);
     }
 
- 
+    // Function to burn tokens, can be called by any token holder
     function burn(uint256 amount) public {
         _burn(msg.sender, amount);
     }
 
-
+    // Function to burn tokens from an account, can only be called by the store contract
     function burnFrom(address account, uint256 amount) public {
         _burn(account, amount);
     }
 
+    // Wrapper function to transfer tokens
     function transferTokens(address to, uint256 amount) public returns (bool) {
         return transfer(to, amount);
     }
